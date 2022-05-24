56309689023aad8873f831ab1dd79c3b50a9d17c
MikesMikes <michaelkaliber15@gmail.com>
2022-05-24 20:41:46 +0100

    first commit

---
+++ src/main/java/github/mikesmikes/manytomanypractice/ManyToManyPracticeApplication.java
@@ -0,0 +1,13 @@
+package github.mikesmikes.manytomanypractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManyToManyPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManyToManyPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/github/mikesmikes/manytomanypractice/ManyToManyPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package github.mikesmikes.manytomanypractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManyToManyPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
